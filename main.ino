39ee3954224a7f64d8e835fc9064cd8758c0ab7c
Boyko Kazakov <boikonur@gmail.com>
2018-03-06 15:51:44 +0200

    Initial Commit

---
+++ main.ino
@@ -0,0 +1,25 @@
+#define CONFIG_PIN1 
+#define CONFIG_PIN2
+
+#define VIN_PIN0
+#define RDIV1
+#define RDIV2
+
+#define LED_PIN0
+#define LED_PIN1
+#define LED_PIN2
+#define LED_PIN3
+#define LED_PIN4
+#define LED_PIN5
+#define LED_PIN6
+#define LED_PIN7
+#define LED_PIN8
+#define LED_PIN9
+
+void setup(){
+
+}
+
+void loop(){
+
+}
